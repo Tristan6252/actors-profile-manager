6e2d3ee5ee09aa75004d79fba84e6987023a45fa
Tristan Norman <20093271@mail.wit.ie>
2023-01-24 18:56:33 +0000

    initialized login and register view

--- app/src/main/java/ie/setu/actorsprofilemanager/ui/login/view/LoginActivity.kt
+++ app/src/main/java/ie/setu/actorsprofilemanager/ui/login/view/LoginActivity.kt
@@ -1,11 +1,12 @@
-package ie.setu.actorsprofilemanager
+package ie.setu.actorsprofilemanager.ui.login.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ie.setu.actorsprofilemanager.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
     }
 }
---
+++ app/src/main/java/ie/setu/actorsprofilemanager/ui/register/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package ie.setu.actorsprofilemanager.ui.register.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.setu.actorsprofilemanager.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_activity)
+    }
+}
