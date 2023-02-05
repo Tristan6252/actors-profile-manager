123bf717c17d8f18fb18a7c6865b1452c22559d2
Tristan Norman <20093271@mail.wit.ie>
2023-02-05 13:58:02 +0000

    add: text alignment for all the fields and their respective tags add: age for each actor instead of the year they were born add: Google Maps API and configured it for an actor to type a city they're born and it displays on the map

--- app/src/main/java/ie/setu/actorsprofilemanager/ui/homepage/model/HomePageModel.kt
+++ app/src/main/java/ie/setu/actorsprofilemanager/ui/homepage/model/HomePageModel.kt
@@ -8,23 +8,23 @@ class HomePageModel : HomePageModelInterface {
 
 
 
-    var actor1 = Actor("Tom Cruise", 'M', LocalDate.parse("1962-08-03"), 6.1, false, "fsfsf1231sdfbb", R.drawable.tomcruise)
-    var actor2 = Actor("Gary Oldman", 'M', LocalDate.parse("1958-03-21"), 5.4, false, "dfdfgdgdfg123", R.drawable.gary_oldman)
-    var actor3 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
+    var actor1 = Actor("Tom Cruise", 'M', LocalDate.parse("1962-08-03"), 6.1, false, "Los Angeles", R.drawable.tomcruise)
+    var actor2 = Actor("Gary Oldman", 'M', LocalDate.parse("1958-03-21"), 5.4, false, "London", R.drawable.gary_oldman)
+    var actor3 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
 
-    var actor4 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor5 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor6 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor7 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor8 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor9 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor10 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor11 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor12 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor13 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor14 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor15 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
-    var actor16 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "a4534534sdf", R.drawable.alecbaldwin)
+    var actor4 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor5 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor6 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor7 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor8 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor9 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor10 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor11 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor12 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor13 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor14 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor15 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
+    var actor16 = Actor("Alec Baldwin", 'M', LocalDate.parse("1958-04-03"), 6.9, true, "New York", R.drawable.alecbaldwin)
 
 
 
--- app/src/main/java/ie/setu/actorsprofilemanager/ui/profile/ActorProfileActivity.kt
+++ app/src/main/java/ie/setu/actorsprofilemanager/ui/profile/ActorProfileActivity.kt
@@ -1,17 +1,23 @@
 package ie.setu.actorsprofilemanager.ui.profile
 
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import ie.setu.actorsprofilemanager.R
 import ie.setu.actorsprofilemanager.models.Actor
 import org.w3c.dom.Text
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 import kotlin.math.log
+import com.google.android.gms.maps.MapsInitializer.Renderer;
 
-class ActorProfileActivity : AppCompatActivity() {
+class ActorProfileActivity : AppCompatActivity(), OnMapReadyCallback, OnMapsSdkInitializedCallback {
 
     private var actorName : TextView? = null
     private var actorGender : TextView? = null
@@ -20,12 +26,21 @@ class ActorProfileActivity : AppCompatActivity() {
     private var actorDeceasedOrNot : TextView? = null
     private var actorBirthPlaceGoogleMaps : TextView? = null
     private var profileImageView : ImageView? = null
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+    private lateinit var actor: Actor
+
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.actor_profile_activity)
+        MapsInitializer.initialize(getApplicationContext(), Renderer.LATEST, this);
         val actor = intent.getSerializableExtra("actor") as? Actor
+        if (actor != null) {
+            this.actor = actor
+        }
         println((actor.toString()))
         actorName = findViewById(R.id.actorName)
         actorName?.text = actor?.name
@@ -33,8 +48,10 @@ class ActorProfileActivity : AppCompatActivity() {
         actorGender = findViewById(R.id.gender)
         actorGender?.text = actor?.gender.toString()
 
+        val today = LocalDate.now()
+        val yearsBetween = ChronoUnit.YEARS.between(actor?.birthDate, today)
         actorBirthDate = findViewById(R.id.birthDate)
-        actorBirthDate?.text = actor?.birthDate.toString()
+        actorBirthDate?.text = yearsBetween.toString()
 
         actorHeight = findViewById(R.id.height)
         actorHeight?.text = actor?.height.toString()
@@ -42,12 +59,41 @@ class ActorProfileActivity : AppCompatActivity() {
         actorDeceasedOrNot = findViewById(R.id.deceased)
         actorDeceasedOrNot?.text = actor?.deceasedOrNot.toString()
 
-        actorBirthPlaceGoogleMaps = findViewById(R.id.googlemaps)
-        actorBirthPlaceGoogleMaps?.text = actor?.birthPlaceGoogleMaps
+//        actorBirthPlaceGoogleMaps = findViewById(R.id.googlemaps)
+//        actorBirthPlaceGoogleMaps?.text = actor?.birthPlaceGoogleMaps
 
         profileImageView = findViewById(R.id.actorProfileImageView)
         if (actor != null) {
             profileImageView?.setImageDrawable(resources.getDrawable(actor.actorProfileImage))
         }
+
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        val geoCoder = Geocoder(this)
+        val cityName = actor.birthPlaceGoogleMaps
+        val addresses = geoCoder.getFromLocationName(cityName, 1)
+        if (addresses != null) {
+            if(addresses.isNotEmpty()) {
+                val address = addresses?.get(0)
+                val latlng = address?.let { LatLng(it.latitude, address.longitude) }
+                latlng?.let { MarkerOptions().position(it).title("Marker in $cityName") }
+                    ?.let { this.googleMap.addMarker(it) }
+                latlng?.let { CameraUpdateFactory.newLatLng(it) }?.let { this.googleMap.moveCamera(it) }
+            }
+        }
+//        val sydney = LatLng(-34.0, 151.0)
+//        this.googleMap.addMarker(MarkerOptions()
+//            .position(sydney)
+//            .title("Marker in Sydney"))
+//        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onMapsSdkInitialized(p0: Renderer) {
+
     }
 }
